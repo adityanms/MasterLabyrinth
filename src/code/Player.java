89090a37f0c86c65f76f2177c3ba272acdc2b053
M.S.Adityan <msadityan@gmail.com>
2016-03-24 17:44:35 -0400

    Added more documentation

--- src/code/Player.java
+++ src/code/Player.java
@@ -6,21 +6,50 @@ public class Player {
 	private Color _color;
 	private int _x, _y;
 	
+	/**
+	 * Constructor
+	 * @param color color of the player
+	 */
 	public Player(Color color){
 		_color = color;
 	}
+	
+	/**
+	 * Returns the x coordinate of the player
+	 * @return
+	 */
 	public int getX(){
 		return _x;
 	}
+	
+	/**
+	 * Returns the y coordinate of the player
+	 * @return
+	 */
 	public int getY(){
 		return _y;
 	}
+	
+	/** 
+	 * Sets the x coordinate of the player
+	 * @param x
+	 */
 	public void setX(int x){
 		_x = x;
 	}
+	
+	/** 
+	 * Sets the y coordinate of the player
+	 * @param y
+	 */
 	public void setY(int y){
 		_y = y;
 	}
+	
+	/**
+	 * Gets the color of the player
+	 * @return
+	 */
 	public Color getColor(){
 		return _color;
 	}
