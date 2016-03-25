4c31c6124267705d8bba2a0c5fe8ed34d53f2fc4
M.S.Adityan <msadityan@gmail.com>
2016-03-25 13:32:31 -0400

    Added code and documentation for Token class

--- src/code/Token.java
+++ src/code/Token.java
@@ -1,5 +1,53 @@
 package code;
+/**
+ * Token class. Each token contains a number 1-20 and 25 and a boolean value which decides
+ * if the token is face down or not.
+ * @author team112
+ *
+ */
 
 public class Token {
-	//TODO
+	
+	private int _tokenNumber;
+	private boolean faceDown;
+	
+	/**
+	 * Constructor for token
+	 * @param number number on the token
+	 */
+	public Token(int number){
+		_tokenNumber = number;
+	}
+	
+	/**
+	 * Getter method for token number
+	 * @return returns the token number
+	 */
+	public int getTokenNumber(){
+		return _tokenNumber;
+	}
+	
+	/**
+	 * Setter method for token number
+	 * @param number number on the token
+	 */
+	public void setTokenNumber(int number){
+		_tokenNumber = number;
+	}
+	
+	/**
+	 * Getter method for boolean value stating if the token is faced down
+	 * @param number the number on the token which is being checked
+	 * @return returns a boolean value stating if the token is faced down
+	 */
+	public boolean isFacedDown(int number){
+		return faceDown;
+	}
+	
+	/**
+	 * Setter method for setting the token to face down
+	 */
+	public void setFaceDown(){
+		faceDown = true;
+	}
 }
